e203c785ddc56eccf7daf1dac190c981219cb550
Gennady Oleshko <gennady@oleshko-g.ru>
2025-02-22 13:04:01 +0300

    add commands: exit, help

--- main.go
+++ main.go
@@ -7,6 +7,51 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commandRegistry map[string]cliCommand
+
+func init() {
+	commandRegistry = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
+func commandUknown() error {
+	fmt.Println("Unknown command")
+	return nil
+}
+
+func commandExit() error {
+	fmt.Print("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+
+	for _, command := range commandRegistry {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	return nil
+}
+
 func cleanInput(text string) []string {
 	t := strings.ToLower(text)
 
@@ -26,6 +71,11 @@ func main() {
 		if len(words) == 0 {
 			continue
 		}
-		fmt.Printf("\rYour command was: %s\n", words[0])
+		command, ok := commandRegistry[words[0]]
+		if !ok {
+			commandUknown()
+			continue
+		}
+		command.callback()
 	}
 }
